3ea1a12d8c7b6b1b78bd284aa79f4d9602e8881e
Ivan Bajalovic <bajalovic@gmail.com>
2012-01-05 23:01:19 +0100

    Izmenjeni config fajlovi

---
+++ kibocms/library/config.sample.php
@@ -0,0 +1,26 @@
+<?php	
+/*	ob_clean();
+	session_start();
+	
+	define("DB_HOST", "localhost");
+	define("DB_USER", "root");
+	define("DB_PASS", "");
+	define("DB_BASE", "kibo_cms");
+	
+	define("DEFAULT_LANG", "eng");
+	
+		
+	function __autoload($class_name) {
+	    include("C:\\xampp\\htdocs\\uskoro\\kibocms\\library\\" . strtolower($class_name) . '.php');
+	}
+	
+	//$_SESSION['lang_id'] = 1;
+	$lang_id = 1;
+*/
+	
+	$folderPath = "kibocms/";
+	include("/home/kiboclou/domains/kibocloud.com/public_html/srdjan/library/config.php");
+	
+	$basePathForUpload = "../../../upload";
+	
+?>
